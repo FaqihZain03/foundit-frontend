1f944e18d707036cbd9cae4b91cc7a62b2ec5150
FaqihZain03 <faqihzain03@gmail.com>
2025-06-24 10:10:03 +0700

    Last Frontend

--- src/components/admin/login.jsx
+++ src/components/admin/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../../styles/admin.css";
+import "../../styles/Admin.css";
 import { loginAdmin } from "../../_services/auth";
 
 const AdminLogin = ({ onLoginSuccess }) => {
